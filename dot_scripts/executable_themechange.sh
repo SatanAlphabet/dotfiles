7f21ae54b82b49f61c0480496c4be8c85dbd0a6b
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-03 19:44:43 +0700

    waybar theme change integration + kde colorschemes

--- dot_scripts/executable_themechange.sh
+++ dot_scripts/executable_themechange.sh
@@ -3,10 +3,14 @@
 current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 if [ $current_theme = "'prefer-dark'" ]; then
   gsettings set org.gnome.desktop.interface gtk-theme 'Gruvbox-Light'
-  dconf write /org/gnome/desktop/interface/color-scheme '"prefer-light"'
+  gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+  sed -i 's/dark/light/' ~/.config/waybar/style.css
+  sed -i 's/dark/light/' ~/.config/rofi/colors.rasi
   notify-send -e -t 3000 "Switched to light mode"
 elif [ $current_theme = "'prefer-light'" ]; then
   gsettings set org.gnome.desktop.interface gtk-theme 'Gruvbox-Dark'
-  dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"'
+  gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+  sed -i 's/light/dark/' ~/.config/waybar/style.css
+  sed -i 's/light/dark/' ~/.config/rofi/colors.rasi
   notify-send -e -t 3000 "Switched to dark mode"
 fi
