972e1abf96a435563823bd89e5a7cab434337934
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-07 03:02:30 +0700

    osd for no player

--- dot_scripts/executable_mediacontrol.sh
+++ dot_scripts/executable_mediacontrol.sh
@@ -5,4 +5,6 @@ if [[ ${status} = 'Playing' ]]; then
   notify-send -e -r 2 -t 1500 "Paused media..."
 elif [[ ${status} = 'Paused' ]]; then
   notify-send -e -r 2 -t 1500 "Playing media..."
+else
+  notify-send -e -r 2 -t 1500 "No media found..."
 fi
