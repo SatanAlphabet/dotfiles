2fd4b29bd4e8ff8f9860db4956c00c04156ae049
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-08 17:41:30 +0700

    WIP matugen integration

--- dot_scripts/executable_wallswitch.sh
+++ dot_scripts/executable_wallswitch.sh
@@ -1,6 +1,7 @@
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
 
 pkill waypaper
+matugen image "$1"
 cp -f "$1" ${cache_dir}/niri/landing/background
 magick "$1" -blur 20x10 ${cache_dir}/niri/landing/blur
 systemctl --user restart overview-blur.service
