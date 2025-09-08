f8db53642455bbd93e92de0cf30a0c5b58d55739
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-08 19:22:16 +0700

    rename matugen config to prevent conflicts

--- dot_scripts/executable_wallswitch.sh
+++ dot_scripts/executable_wallswitch.sh
@@ -1,7 +1,7 @@
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
 
 pkill waypaper
-matugen image "$1"
+matugen image "$1" -c "~/.config/matugen/main.toml"
 cp -f "$1" ${cache_dir}/niri/landing/background
 magick "$1" -blur 20x10 ${cache_dir}/niri/landing/blur
 systemctl --user restart overview-blur.service
