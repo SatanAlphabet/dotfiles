817089cd340252577a9ea57f69eb56d7897cc37e
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-09 15:17:44 +0700

    more matugen integration

--- dot_scripts/executable_themechange.sh
+++ dot_scripts/executable_themechange.sh
@@ -5,6 +5,7 @@ wallpaper=$(waypaper --list | jq '.[].wallpaper' | sed 's/"//g')
 
 if [ $current_theme = "'prefer-dark'" ]; then
 
+  niri msg action do-screen-transition
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
   matugen image ${wallpaper} -c ~/.config/matugen/main.toml -m light
 
@@ -13,26 +14,17 @@ if [ $current_theme = "'prefer-dark'" ]; then
   #sed -i 's/dark/light/' ~/.config/wlogout/style_1.css
   #sed -i 's/dark.css/light.css/' ~/.config/swaync/style.css
 
-  cat ~/.config/cava/themes/gruv-mat-light >~/.config/cava/config
-  pkill -SIGUSR1 cava
-
-  swaync-client -rs
-
   notify-send -e -t 3000 "Switched to light mode"
 
 elif [ $current_theme = "'prefer-light'" ]; then
 
+  niri msg action do-screen-transition
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
   matugen image ${wallpaper} -c ~/.config/matugen/main.toml -m dark
 
   #sed -i 's/light/dark/' ~/.config/rofi/colors.rasi
   #sed -i 's/light/dark/' ~/.config/wlogout/style_1.css
   #sed -i 's/light.css/dark.css/' ~/.config/swaync/style.css
-
-  cat ~/.config/cava/themes/gruv-mat-dark >~/.config/cava/config
-  pkill -SIGUSR1 cava
-
-  swaync-client -rs
 
   notify-send -e -t 3000 "Switched to dark mode"
 
