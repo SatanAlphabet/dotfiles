d4709ed9f23e11cdc92e1ebfd5688a724e049345
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-09 12:50:48 +0700

    theme change rework

--- dot_scripts/executable_wallswitch.sh
+++ dot_scripts/executable_wallswitch.sh
@@ -1,7 +1,14 @@
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
+current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 
 pkill waypaper
-matugen image "$1" -c ~/.config/matugen/main.toml
+
+if [ $current_theme = "'prefer-dark'" ]; then
+  matugen image "$1" -c ~/.config/matugen/main.toml -m dark
+elif [ $current_theme = "'prefer-light'" ]; then
+  matugen image "$1" -c ~/.config/matugen/main.toml -m light
+fi
+
 cp -f "$1" ${cache_dir}/niri/landing/background
 magick "$1" -blur 20x10 ${cache_dir}/niri/landing/blur
 systemctl --user restart overview-blur.service
