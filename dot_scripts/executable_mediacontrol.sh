2a4808ecceabd0f18a3aa40d1ce1fc24bd283523
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-17 00:50:57 +0700

    more thing

--- dot_scripts/executable_mediacontrol.sh
+++ dot_scripts/executable_mediacontrol.sh
@@ -1,10 +1,23 @@
-status=$(playerctl status)
+status=$(playerctl status 2>/dev/null)
 
-playerctl play-pause
-if [[ ${status} = 'Playing' ]]; then
-  notify-send -e -r 2 -t 1500 "Paused media..."
-elif [[ ${status} = 'Paused' ]]; then
-  notify-send -e -r 2 -t 1500 "Playing media..."
+if [[ -z ${status} ]]; then
+  notify-send "No meadia found..." -e -r 4 -t 1500
+  exit 0
+fi
+
+if [[ "$1" = 'next' ]]; then
+  playerctl next 2>/dev/null
+  notify-send -e -r 4 -t 1500 'Playing next track...'
+elif [[ "$1" = 'prev' ]]; then
+  playerctl previous 2>/dev/null
+  notify-send -e -r 4 -t 1500 'Playing previous track...'
+elif [[ "$1" = 'toggle' ]]; then
+  playerctl play-pause
+  if [[ ${status} = 'Playing' ]]; then
+    notify-send -e -r 4 -t 1500 "Paused media..."
+  elif [[ ${status} = 'Paused' ]]; then
+    notify-send -e -r 4 -t 1500 "Playing media..."
+  fi
 else
-  notify-send -e -r 2 -t 1500 "No media found..."
+  echo "Invalid command (Available commands are \"prev\", \"next\" and \"toggle\")."
 fi
