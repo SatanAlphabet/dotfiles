d88092d4f7814a8f2c45941ed105ee35506ef057
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-18 18:48:03 +0700

    rofi & waybar adjustment

--- dot_scripts/executable_calc.sh
+++ dot_scripts/executable_calc.sh
@@ -7,5 +7,7 @@ if [ "$rofi_running" -ne 0 ]; then
 else
   rofi -modi calc -show calc -no-show-match -no-sort -theme style_4 \
     -theme-str "entry { placeholder: \"Calculate...\"; }" \
-    -calc-command "echo -n '{result}' | wl-copy && notify-send \"Result copied to clipboard...\" -e"
+    -calc-command "echo -n '{result}' | wl-copy && notify-send \"Result copied to clipboard...\" -e" \
+    -theme-str "configuration { calc { hint-welcome: \" Ctrl-Enter to copy current result to clipboard.\"; } } " \
+    -theme-str "mode-switcher { enabled: false; } "
 fi
