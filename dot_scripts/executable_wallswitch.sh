ca811fa20de323fa207cb9913991c69e8c4d57a5
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-19 20:21:44 +0700

    matugen prep #2

--- dot_scripts/executable_wallswitch.sh
+++ dot_scripts/executable_wallswitch.sh
@@ -1,8 +1,8 @@
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
+current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 
-pkill waypaper
-matugen image "$1"
-cp -f "$1" ${cache_dir}/niri/landing/background
-magick "$1" -blur 20x10 ${cache_dir}/niri/landing/blur
-systemctl --user restart overview-blur.service
-notify-send -e -r 2 -t 2000 "Wallpaper switch successful..." "$1"
+if [ $current_theme = "'prefer-dark'" ]; then
+  matugen image "$1" -c ~/.config/matugen/main.toml -m dark
+elif [ $current_theme = "'prefer-light'" ]; then
+  matugen image "$1" -c ~/.config/matugen/main.toml -m light
+fi
