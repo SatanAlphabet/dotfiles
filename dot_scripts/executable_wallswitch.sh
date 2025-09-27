0bf1b5fd20ef933c28dd2e74fe6058f3b55ff2ed
SatanAlphabet <ori0pppppp@gmail.com>
2025-09-28 01:06:33 +0700

    waybar reformat + no longer kill waypaper on wallswitch

--- dot_scripts/executable_wallswitch.sh
+++ dot_scripts/executable_wallswitch.sh
@@ -1,8 +1,6 @@
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
 current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 
-pkill waypaper
-
 if [ $current_theme = "'prefer-dark'" ]; then
   matugen image "$1" -c ~/.config/matugen/main.toml -m dark
 elif [ $current_theme = "'prefer-light'" ]; then
