d94a04f9cd78918c1429660ab241ca2637a017c4
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-01 02:01:38 +0700

    rework update script

---
+++ dot_scripts/executable_sysupdate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+fastfetch
+echo '''
+
+ ===>  Beginning system update...
+
+        '''
+paru -Syu
+echo '''
+
+ ===>  Checking for flatpak updates...
+        
+        '''
+flatpak update
+echo '''
+
+ ===>  Running post-installation checks...
+
+        '''
+sudo checkservices
+notify-send 'System update completed...' -a 'System Update'
+read -n 1 -p 'Press any key to exit...'
