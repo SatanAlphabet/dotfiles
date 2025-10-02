013ddd37e619858fa82f072a235edab38c779e95
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-02 22:55:24 +0700

    niri config & template adjustment

--- dot_scripts/executable_wallswitch.sh
+++ dot_scripts/executable_wallswitch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
+blur_img="${cache_dir}/niri/landing/blur"
 current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 
 if [ "$current_theme" = "'prefer-dark'" ]; then
@@ -9,6 +10,8 @@ elif [ "$current_theme" = "'prefer-light'" ]; then
 fi
 
 cp -sf "$1" "${cache_dir}"/niri/landing/background
-magick "$1" -blur 20x10 "${cache_dir}"/niri/landing/blur
+magick "$1" -blur 20x10 "${blur_img}"
+# waypaper doesn't work properly with multiple swww-daemon instances
+# swww img -n overview --transition-duration 2 -t fade "${blur_img}"
 systemctl --user restart overview-blur.service
 notify-send -e -r 2 -t 2000 "Wallpaper switch successful..." "$1"
