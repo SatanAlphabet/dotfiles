02c6113dab1b037155b7ca01ed29790c95c2b58f
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-03 11:43:56 +0700

    improve mediacontrol script

--- dot_scripts/executable_mediacontrol.sh
+++ dot_scripts/executable_mediacontrol.sh
@@ -2,16 +2,28 @@
 status=$(playerctl status 2>/dev/null)
 
 if [[ -z ${status} ]]; then
-  notify-send "No meadia found..." -e -r 4 -t 1500
+  notify-send "No media found..." -e -r 4 -t 1500
   exit 0
 fi
 
 media_data="$(playerctl metadata title)"
-current_pos=$(printf "%0.f" $(playerctl position))
+current_pos=$(printf "%0.f" "$(playerctl position)")
 media_length=$(playerctl metadata mpris:length)
 
-media_length=$((media_length / 60 / 1000 / 1000))
-media_progress=$((current_pos / 60 * 100 / media_length))
+media_progress=$((current_pos * 100 * 1000000 / media_length))
+
+## WIP
+# length_sec=$((media_length / 1000000))
+# length_min=$((media_length / 1000000 / 60))
+# parsed_sec=$((length_sec - (length_min * 60)))
+# pos_min=$((current_pos / 60))
+# pos_sec=$((current_pos - (pos_min * 60)))
+# if [[ $pos_sec -lt 10 ]]; then
+#   pos_sec="0${pos_sec}"
+# fi
+# if [[ $parsed_sec -lt 10 ]]; then
+#   parsed_sec="0${parsed_sec}"
+# fi
 
 if [[ "$1" = 'next' ]]; then
   playerctl next 2>/dev/null
@@ -22,7 +34,7 @@ elif [[ "$1" = 'prev' ]]; then
 elif [[ "$1" = 'toggle' ]]; then
   playerctl play-pause
   if [[ ${status} = 'Playing' ]]; then
-    notify-send -e -r 4 -t 1500 "Paused media [ 󰏤 ]" "$media_data" -h int:value:"$media_progress"
+    notify-send -e -r 4 -t 1500 -u low "Paused media [ 󰏤 ]" "$media_data" -h int:value:"$media_progress"
   elif [[ ${status} = 'Paused' ]]; then
     notify-send -e -r 4 -t 1500 "Playing media [ 󰝚 ]" "$media_data" -h int:value:"$media_progress"
   fi
