6214c74f15962abca1b9ba976c3ed278e535662c
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-05 22:48:13 +0700

    stop tracking gtk + install script update

---
+++ .chezmoiscripts/run_once_after_10-post-install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo -e "===>  Starting post-setup configuration...\n"
+
+_install_confirm() {
+  local response
+  echo -n -e "$1"
+  read -r response
+
+  if [[ "$response" =~ ^[Yy]$|^$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+services=(swayidle-niri wl-clip-persist niriusd wl-paste-text-niri wl-paste-image-niri)
+if _install_confirm "Add necessary systemd services? [Y/n] " -eq 0; then
+  echo -e "===>  Adding necessary user services..."
+  for service in "${services[@]}"; do
+    echo "- Adding: ""$service"".service"
+    systemctl --user add-wants niri.service "$service"
+  done
+fi
+
+if [[ ! -d "$HOME/.cache/niri/landing" ]]; then
+  echo -e "===>  Creating cache folder..."
+  mkdir -p "$HOME/.cache/niri/landing/"
+else
+  echo -e "===>  Cache folder exist. Skipping..."
+fi
+
+if _install_confirm "Build xdg-menu cache? [Y/n]" -eq 0; then
+  echo -e "===> Building xdg-menu cache..."
+  XDG_MENU_PREFIX=plasma- kbuildsyscoca6 --noincremental >/dev/null
+fi
+
+if _install_confirm "Change GTK settings? [Y/n]" -eq 0; then
+  gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+  gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
+  gsettings set org.gnome.desktop.interface font-name 'Inter 10'
+  gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
+  gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
+fi
+
+echo -e "===>  Basic setup installed."
+echo -e "Add 'post_command = ~/.scripts/wallswitch.sh \$wallpaper' to your waypaper's config.ini"
+echo -e "Also setup your system theme with qt6ct & nwg-look and do a restart to run the services."
---
+++ .chezmoiscripts/run_once_before_00-install-pkg.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo -e "===>  Starting setup installation...\n"
+
+_install_confirm() {
+  local response
+  echo -n -e "$1"
+  read -r response
+
+  if [[ "$response" =~ ^[Yy]$|^$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if ! command -v paru &>/dev/null; then
+  echo -e "===>  paru is not installed. Installing paru..."
+  sudo pacman -S --needed base-devel
+  git clone https://aur.archlinux.org/paru-bin.git ./paru
+  cd paru || exit 1
+  makepkg -si
+  cd ..
+  rm -rf paru
+else
+  echo -e "===>  paru is installed. Skipping..."
+fi
+
+pkg_path="$HOME/.local/share/chezmoi"
+if _install_confirm "Install base packages? [Y/n] " -eq 0; then
+  echo -e "\n===>  Installing base packages...\n"
+  paru -S --needed - <"$pkg_path/package.list"
+fi
+
+if _install_confirm "Install AUR packages? [Y/n] " -eq 0; then
+  echo -e "\n===>  Installing AUR packages...\n"
+  paru -S --needed --confirm - <"$pkg_path/package-aur.list"
+fi
+
+if _install_confirm "Switch shell to zsh? [Y/n] " -eq 0; then
+  echo -e "\n===>  Switching shell to zsh...\n"
+  chsh -s "$(which zsh)"
+fi
