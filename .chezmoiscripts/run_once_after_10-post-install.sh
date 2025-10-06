fe81b702e3b0e3c588732777113bfb2a9b24af4d
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-07 01:10:53 +0700

    more install script improvements

--- .chezmoiscripts/run_once_after_10-post-install.sh
+++ .chezmoiscripts/run_once_after_10-post-install.sh
@@ -21,9 +21,9 @@ _install_confirm() {
 services=(swayidle-niri wl-clip-persist niriusd wl-paste-text-niri wl-paste-image-niri)
 if _install_confirm "Add necessary systemd services? [Y/n] " -eq 0; then
   echo -e "===>  Adding necessary user services..."
+  systemctl --user daemon-reload
   for service in "${services[@]}"; do
-    echo "- Adding: ""$service"".service"
-    systemctl --user add-wants niri.service "$service"
+    systemctl --user add-wants niri.service "$service" && echo "===>  Added ""$service"".service"
   done
 fi
 
@@ -35,28 +35,36 @@ else
 fi
 
 if _install_confirm "Build xdg-menu cache? [Y/n] " -eq 0; then
-  echo -e "===> Building xdg-menu cache..."
+  echo -e "===>  Building xdg-menu cache..."
   XDG_MENU_PREFIX=plasma- kbuildsyscoca6 --noincremental >/dev/null
 fi
 
 if _install_confirm "Change GTK settings? [Y/n] " -eq 0; then
-  gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-  gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
-  gsettings set org.gnome.desktop.interface font-name 'Inter 10'
-  gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
-  gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
+  echo -e "===>  Changing GTK settings..."
+  device_theme="prefer-light"
+  cursor_theme="Bibata-Modern-Ice"
+  font="Inter 10"
+  gtk_theme="adw-gtk3"
+  icon_theme="Adwaita"
+  gsettings set org.gnome.desktop.interface color-scheme "${device_theme}" && echo -e "===>  Device theme set to ${device_theme}"
+  gsettings set org.gnome.desktop.interface cursor-theme "${cursor_theme}" && echo -e "===>  Cursor theme set to ${cursor_theme}"
+  gsettings set org.gnome.desktop.interface font-name "${font}" && echo -e "===>  Font set to ${font}"
+  gsettings set org.gnome.desktop.interface gtk-theme "${gtk_theme}" && echo -e "===>  GTK theme set to ${gtk_theme}"
+  gsettings set org.gnome.desktop.interface icon-theme "${icon_theme}" && echo -e "===>  Icon theme set to ${icon_theme}"
 fi
 
 if _install_confirm "Setup waypaper config? [Y/n] " -eq 0; then
   mkdir -p "$HOME/.config/waypaper/"
-  cp "$pkg_path/waypaper-config.ini" "$HOME/.config/waypaper/config.ini"
+  cp "$pkg_path/waypaper-config.ini" "$HOME/.config/waypaper/config.ini" && echo -e "===>  Copied starting waypaper config..."
 fi
 
-if _install_confirm "Set default background and run matugen? [Y/n] " -eq 0; then
+if _install_confirm "Set default background and run matugen? (REQUIRES WAYPAPER CONFIGURED) [Y/n] " -eq 0; then
+  echo -e "===>  Copying default background to wallpaper directory... (${wall_path})"
   mkdir -p "$wall_path/"
   cp "$pkg_path/default-bg.png" "$wall_path/default-bg.png"
+  echo -e "===>  Applying changes with waypaper..."
   waypaper --wallpaper "$wall_path/default-bg.png"
 fi
 
-echo -e "===>  Basic setup installed."
+echo -e "===>  Basic setup completed."
 echo -e "Setup your system theme with qt6ct & nwg-look and do a restart to run the services."
