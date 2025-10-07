9c5895359294dbd45cd207ceec996800d57e040d
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-08 00:44:23 +0700

    format update

--- dot_config/zsh/prompt.zsh
+++ dot_config/zsh/prompt.zsh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
 if [[ -r $ZDOTDIR/.p10k.zsh && -f $ZDOTDIR/.p10k.zsh ]]; then
-    # ===== START Initialize Powerlevel10k theme =====
-    source $ZDOTDIR/.p10k.zsh
+  # ===== START Initialize Powerlevel10k theme =====
+  source $ZDOTDIR/.p10k.zsh
     # ===== END Initialize Powerlevel10k theme =====
 elif command -v starship &>/dev/null; then
-    # ===== START Initialize Starship prompt =====
-    eval "$(starship init zsh)"
-    export STARSHIP_CACHE=$XDG_CACHE_HOME/starship
-    export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
-    # ===== END Initialize Starship prompt =====
+  # ===== START Initialize Starship prompt =====
+  eval "$(starship init zsh)"
+  export STARSHIP_CACHE=$XDG_CACHE_HOME/starship
+  export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
+  # ===== END Initialize Starship prompt =====
 fi
