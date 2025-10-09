b230fc1bb641440bf8d96d3de5cc00f5d9e63294
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-09 10:29:57 +0700

    new keyboard layout toggle bind & script

---
+++ dot_scripts/executable_layout-toggle.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+case "$1" in
+"prev")
+  niri msg action switch-layout prev
+  ;;
+"next")
+  niri msg action switch-layout next
+  ;;
+*)
+  echo "Invalid options. (Supported options are \"next\" and \"prev\")." >&2
+  exit 1
+  ;;
+esac
+
+idx=$(niri msg -j keyboard-layouts | jq '.current_idx')
+current_layout=$(niri msg -j keyboard-layouts | jq .names["$idx"] | sed 's/"//g')
+
+notify-send -a "keyboard-layout" -r 9 -e -t 2000 "Current layout: $current_layout"
