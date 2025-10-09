ba8bdcabba9fab071aa673ea1a1bc31a8cada547
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-09 13:37:15 +0700

    volumecontrol: add mute functions and bind

--- dot_scripts/executable_volumecontrol.sh
+++ dot_scripts/executable_volumecontrol.sh
@@ -7,12 +7,27 @@ case "$1" in
 -)
   pactl set-sink-volume @DEFAULT_SINK@ -"${2}"%
   ;;
+mute)
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ 1
+  ;;
+unmute)
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
+  ;;
+toggle-mute)
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+  ;;
 *)
   exit 1
   ;;
 esac
 
-# Get the volume level and convert it to a percentage
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d\n", $2*100}')
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d\n", $2*100}')"%"
+mute_status=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%s\n", $3}')
+priority="normal"
 
-notify-send -t 2000 -a "volume" -r 2 -e "Volume level: ${volume}%" -h int:value:"$volume"
+if [[ "$mute_status" == "[MUTED]" ]]; then
+  volume="$volume"" (Muted)"
+  priority="low"
+fi
+
+notify-send -t 2000 -a "volume" -u "$priority" -r 2 -e "Volume level: ${volume}" -h int:value:"$volume"
