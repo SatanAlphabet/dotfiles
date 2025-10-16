96d4b2a75769453cdf6363c4a8ccac9790529095
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-16 13:36:27 +0700

    refactor mediacontrol.sh

--- private_dot_local/bin/executable_mediacontrol.sh
+++ private_dot_local/bin/executable_mediacontrol.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
-status=$(playerctl status 2>/dev/null)
 
-if [[ -z ${status} ]]; then
-  notify-send "No media found..." -e -r 1 -t 1500 -u low
-  exit 0
-fi
+media_check() {
+  status=$(playerctl status 2>/dev/null)
+  if [[ -z ${status} ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
 
-media_data="$(playerctl metadata title)"
-current_pos=$(printf "%0.f" "$(playerctl position)")
-media_length=$(playerctl metadata mpris:length)
-
-media_progress=$((current_pos * 100 * 1000000 / media_length))
+media_progress() {
+  local current_pos
+  local media_length
+  current_pos=$(printf "%0.f" "$(playerctl position)")
+  media_length=$(playerctl metadata mpris:length)
+  local current_progress=$((current_pos * 100 * 1000000 / media_length))
+  echo "$current_progress"
+}
 
 ## WIP
 # length_sec=$((media_length / 1000000))
@@ -25,19 +31,37 @@ media_progress=$((current_pos * 100 * 1000000 / media_length))
 #   parsed_sec="0${parsed_sec}"
 # fi
 
-if [[ "$1" = 'next' ]]; then
-  playerctl next 2>/dev/null
-  notify-send -e -r 1 -t 1500 'Playing next track...'
-elif [[ "$1" = 'prev' ]]; then
-  playerctl previous 2>/dev/null
-  notify-send -e -r 1 -t 1500 'Playing previous track...'
-elif [[ "$1" = 'toggle' ]]; then
-  playerctl play-pause
-  if [[ ${status} = 'Playing' ]]; then
-    notify-send -e -r 1 -t 1500 "Paused media [ 󰏤 ]" "$media_data" -h int:value:"$media_progress"
-  elif [[ ${status} = 'Paused' ]]; then
-    notify-send -e -r 1 -t 1500 "Playing media [ 󰝚 ]" "$media_data" -h int:value:"$media_progress"
+case "$1" in
+'next')
+  if media_check -eq 0; then
+    playerctl next 2>/dev/null
+    notify-send -e -r 1 -t 1500 'Playing next track...'
+  else
+    notify-send "No media found..." -e -r 1 -t 1500 -u low
   fi
-else
+  ;;
+'prev')
+  if media_check -eq 0; then
+    playerctl previous 2>/dev/null
+    notify-send -e -r 1 -t 1500 'Playing previous track...'
+  else
+    notify-send "No media found..." -e -r 1 -t 1500 -u low
+  fi
+  ;;
+'toggle')
+  if media_check -eq 0; then
+    media_data="$(playerctl metadata title)"
+    playerctl play-pause
+    if [[ ${status} = 'Playing' ]]; then
+      notify-send -e -r 1 -t 1500 "Paused media [ 󰏤 ]" "$media_data" -h int:value:"$(media_progress)"
+    elif [[ ${status} = 'Paused' ]]; then
+      notify-send -e -r 1 -t 1500 "Playing media [ 󰝚 ]" "$media_data" -h int:value:"$(media_progress)"
+    else
+      notify-send "No media found..." -e -r 1 -t 1500 -u low
+    fi
+  fi
+  ;;
+*)
   echo "Invalid command (Available commands are \"prev\", \"next\" and \"toggle\")."
-fi
+  ;;
+esac
