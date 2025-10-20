b9935d7aaae663058a3eaf6216b3bea8e644ebef
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-20 22:33:50 +0700

    thing

--- private_dot_local/bin/executable_matugen-pywalfox.sh
+++ private_dot_local/bin/executable_matugen-pywalfox.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 
+if ! pgrep -x firefox >/dev/null; then
+  exit 1
+fi
+
 if [ "$current_theme" = "'prefer-dark'" ]; then
   pywalfox dark
 elif [ "$current_theme" = "'prefer-light'" ]; then
