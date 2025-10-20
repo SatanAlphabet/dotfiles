763c824bba8e1e0bf27a7eb9c606793857a5299c
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-21 00:08:17 +0700

    nightlight: accept temp args

--- private_dot_local/bin/executable_nightlight.sh
+++ private_dot_local/bin/executable_nightlight.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
+temp=${1:-4000}
+
 if pgrep -x wlsunset >/dev/null; then
   pkill wlsunset && notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Disabled."
 else
-  wlsunset >/dev/null 2>&1 &
+  wlsunset -t "$temp" >/dev/null 2>&1 &
   notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light enabled..."
 fi
