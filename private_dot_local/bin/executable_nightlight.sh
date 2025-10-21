2ab563d116f1f97521ad7fb33afc92a3234f679b
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-22 00:15:07 +0700

    nightlight.sh: improve notif

--- private_dot_local/bin/executable_nightlight.sh
+++ private_dot_local/bin/executable_nightlight.sh
@@ -3,8 +3,8 @@
 temp=${1:-4000}
 
 if pgrep -x wlsunset >/dev/null; then
-  pkill wlsunset && notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Disabled."
+  pkill wlsunset && notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Disabled..."
 else
   wlsunset -t "$temp" >/dev/null 2>&1 &
-  notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light enabled..."
+  notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Enabled..." "Temperature: ${temp}K"
 fi
