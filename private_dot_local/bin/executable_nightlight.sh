fed16d030e36bb50feaf7e71b074be38d6ef1d3d
SatanAlphabet <ori0pppppp@gmail.com>
2025-10-25 00:30:35 +0700

    nightlight.sh: add signal to update waybar

--- private_dot_local/bin/executable_nightlight.sh
+++ private_dot_local/bin/executable_nightlight.sh
@@ -8,3 +8,5 @@ else
   wlsunset -t "$temp" >/dev/null 2>&1 &
   notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Enabled..." "Temperature: ${temp}K"
 fi
+
+pgrep -x "waybar" >/dev/null && pkill -RTMIN+8 "waybar"
