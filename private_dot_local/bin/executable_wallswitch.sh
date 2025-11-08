aad68a4a2c95ed4c519df761004bf5e3e89a3a01
SatanAlphabet <ori0pppppp@gmail.com>
2025-11-09 00:01:52 +0700

    wallswitch: filepath check + small notif change

--- private_dot_local/bin/executable_wallswitch.sh
+++ private_dot_local/bin/executable_wallswitch.sh
@@ -3,6 +3,11 @@ cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
 blur_img="${cache_dir}/niri/landing/blur"
 current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 
+if [[ ! -f "$1" ]]; then
+  echo "ERROR: $1 is not a valid file" >&2
+  exit 1
+fi
+
 if [ "$current_theme" = "'prefer-dark'" ]; then
   matugen image "$1" -m dark
 elif [ "$current_theme" = "'prefer-light'" ]; then
@@ -14,4 +19,4 @@ magick "$1" -blur 20x10 "${blur_img}"
 # waypaper doesn't work properly with multiple swww-daemon instances
 # swww img -n overview --transition-duration 2 -t fade "${blur_img}"
 systemctl --user restart overview-blur.service
-notify-send -e -r 2 -t 2000 "Wallpaper switch successful..." "$1"
+notify-send -e -r 2 -t 2000 "Wallpaper switch successful..." "Current Wallpaper: $(basename "$1")"
