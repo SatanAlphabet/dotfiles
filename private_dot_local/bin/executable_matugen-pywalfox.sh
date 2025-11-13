f077962c59db78273dc2f278a362e318627d0fb1
SatanAlphabet <ori0pppppp@gmail.com>
2025-11-13 12:35:12 +0700

    matugen: pywalfox post-hook always run

--- private_dot_local/bin/executable_matugen-pywalfox.sh
+++ private_dot_local/bin/executable_matugen-pywalfox.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 
-if ! pgrep -x firefox >/dev/null; then
-  exit 1
-fi
-
 if [ "$current_theme" = "'prefer-dark'" ]; then
   pywalfox dark
 elif [ "$current_theme" = "'prefer-light'" ]; then
