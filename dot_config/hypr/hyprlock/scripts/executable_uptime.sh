8a739afc73b43fc0a363a4c4401aa8de05536a91
SatanAlphabet <ori0pppppp@gmail.com>
2025-11-15 01:44:05 +0700

    hyprlock stuff
    - several theme-2 adjustments
    - custom scripts + directory
    - variable cleanups

---
+++ dot_config/hypr/hyprlock/scripts/executable_uptime.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ "$(cat /proc/uptime | awk '{printf ("%d",($1/1))}')" -lt 86400 ]; then
+  awk '{printf("Uptime: %dh %02dm\n",($1/60/60%24),($1/60%60))}' /proc/uptime
+else
+  awk '{printf("Uptime: %dd %dh %02dm\n",($1/60/60/24),($1/60/60%24),($1/60%60))}' /proc/uptime
+fi
