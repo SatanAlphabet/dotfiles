a26e3af9beea3a7a4dedc9c4d4c94de9552f9c07
SatanAlphabet <ori0pppppp@gmail.com>
2025-11-17 19:35:43 +0700

    wallpaper + matugen script improvement
    - skip running matugen & generating overview bg if same bg is detected
    - themechange.sh no longer hardcoded to waypaper bg

--- private_dot_local/bin/executable_themechange.sh
+++ private_dot_local/bin/executable_themechange.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
-wallpaper=$(waypaper --list | jq -r '.[].wallpaper')
+wallpaper="$1"
+
+if [ ! -f "$wallpaper" ]; then
+  echo "Error: image $1 was not found."
+  exit 1
+fi
 
 if [ "$current_theme" = "'prefer-dark'" ]; then
 
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-  matugen image "${wallpaper}" -m light
+  matugen image "$wallpaper" -m light
   notify-send -e -t 3000 "Switched to light mode"
 
 elif [ "$current_theme" = "'prefer-light'" ]; then
 
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-  matugen image "${wallpaper}" -m dark
+  matugen image "$wallpaper" -m dark
   notify-send -e -t 3000 "Switched to dark mode"
 
 fi
--- private_dot_local/bin/executable_wallswitch.sh
+++ private_dot_local/bin/executable_wallswitch.sh
@@ -8,15 +8,19 @@ if [[ ! -f "$1" ]]; then
   exit 1
 fi
 
-if [ "$current_theme" = "'prefer-dark'" ]; then
-  matugen image "$1" -m dark
-elif [ "$current_theme" = "'prefer-light'" ]; then
-  matugen image "$1" -m light
-fi
+if [[ "$1" != "$(readlink -f "$cache_dir/niri/landing/background")" ]]; then
+  if [ "$current_theme" = "'prefer-dark'" ]; then
+    matugen image "$1" -m dark
+  elif [ "$current_theme" = "'prefer-light'" ]; then
+    matugen image "$1" -m light
+  fi
 
-cp -sf "$1" "${cache_dir}"/niri/landing/background
-magick "$1" -blur 20x10 "${blur_img}"
-# waypaper doesn't work properly with multiple swww-daemon instances
-# swww img -n overview --transition-duration 2 -t fade "${blur_img}"
+  cp -sf "$1" "${cache_dir}"/niri/landing/background
+  magick "$1" -blur 20x10 "${blur_img}"
+  # waypaper doesn't work properly with multiple swww-daemon instances
+  # swww img -n overview --transition-duration 2 -t fade "${blur_img}"
+else
+  echo "Same wallpaper found. Skipping matugen & caching..."
+fi
 systemctl --user restart overview-blur.service
 notify-send -e -r 2 -t 2000 "Wallpaper switch successful..." "Current Wallpaper: $(basename "$1")"
