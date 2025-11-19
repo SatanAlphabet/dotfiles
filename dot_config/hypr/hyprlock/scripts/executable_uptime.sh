d690d87f8452ba7e7bf7b987970c088570a0880a
SatanAlphabet <ori0pppppp@gmail.com>
2025-11-19 12:16:27 +0700

    hyprlock: update uptime.sh
    - only prints minute if uptime < 1 hr

--- dot_config/hypr/hyprlock/scripts/executable_uptime.sh
+++ dot_config/hypr/hyprlock/scripts/executable_uptime.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-if [ "$(cat /proc/uptime | awk '{printf ("%d",($1/1))}')" -lt 86400 ]; then
-  awk '{printf("%dh %02dm\n",($1/60/60%24),($1/60%60))}' /proc/uptime
+uptime="$(cat /proc/uptime | awk '{printf ("%d",($1/1))}')"
+
+if [ "$uptime" -lt 3600 ]; then
+  awk '{printf("%dm\n",($1/60%60))}' <<<"$uptime"
+elif [ "$uptime" -lt 86400 ]; then
+  awk '{printf("%dh %02dm\n",($1/60/60%24),($1/60%60))}' <<<"$uptime"
 else
-  awk '{printf("%dd %dh %02dm\n",($1/60/60/24),($1/60/60%24),($1/60%60))}' /proc/uptime
+  awk '{printf("%dd %dh %02dm\n",($1/60/60/24),($1/60/60%24),($1/60%60))}' <<<"$uptime"
 fi
