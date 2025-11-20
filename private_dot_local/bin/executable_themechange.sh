0aa0911682ab22142acf1e6d05366f1c6d83cb1e
SatanAlphabet <ori0pppppp@gmail.com>
2025-11-21 00:13:33 +0700

    themechange.sh: add icon support

--- private_dot_local/bin/executable_themechange.sh
+++ private_dot_local/bin/executable_themechange.sh
@@ -12,12 +12,12 @@ if [ "$current_theme" = "'prefer-dark'" ]; then
 
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
   matugen image "$wallpaper" -m light
-  notify-send -e -t 3000 "Switched to light mode"
+  notify-send -e -t 3000 "Switched to light mode..." -i weather-clear-symbolic
 
 elif [ "$current_theme" = "'prefer-light'" ]; then
 
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
   matugen image "$wallpaper" -m dark
-  notify-send -e -t 3000 "Switched to dark mode"
+  notify-send -e -t 3000 "Switched to dark mode..." -i weather-clear-night-symbolic
 
 fi
