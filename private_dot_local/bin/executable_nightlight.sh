0ab70ea2c7744fdbb3dbc303f57d4e57bd538666
SatanAlphabet <ori0pppppp@gmail.com>
2025-11-20 11:52:02 +0700

    more icon supports in custom notif
    - icons in unlock notif, brightnesscontrol.sh and nightlight.sh

--- private_dot_local/bin/executable_nightlight.sh
+++ private_dot_local/bin/executable_nightlight.sh
@@ -3,10 +3,10 @@
 temp=${1:-4000}
 
 if pgrep -x wlsunset >/dev/null; then
-  pkill wlsunset && notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Disabled..."
+  pkill wlsunset && notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Disabled..." -i night-light-symbolic
 else
   wlsunset -t "$temp" >/dev/null 2>&1 &
-  notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Enabled..." "Temperature: ${temp}K"
+  notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Enabled..." "Temperature: ${temp}K" -i night-light-symbolic
 fi
 
 pgrep -x "waybar" >/dev/null && pkill -RTMIN+8 "waybar"
