0e63f56aacb69fab5fbb9243b23e032188887a81
SatanAlphabet <ori0pppppp@gmail.com>
2025-11-22 00:32:13 +0700

    minor stuff idk
    - wallswitch.sh: use ln instead of cp
    - yazi: use $EDITOR instead of forcing nvim

--- private_dot_local/bin/executable_wallswitch.sh
+++ private_dot_local/bin/executable_wallswitch.sh
@@ -15,7 +15,7 @@ if [[ "$1" != "$(readlink -f "$cache_dir/niri/landing/background")" ]]; then
     matugen image "$1" -m light
   fi
 
-  cp -sf "$1" "${cache_dir}"/niri/landing/background
+  ln -sf "$1" "${cache_dir}"/niri/landing/background
   magick "$1" -blur 20x10 "${blur_img}"
   # waypaper doesn't work properly with multiple swww-daemon instances
   # swww img -n overview --transition-duration 2 -t fade "${blur_img}"
