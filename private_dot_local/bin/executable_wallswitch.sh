7d8288aba8f6d3b34ed6da1b4de376cb5e23bc82
SatanAlphabet <ori0pppppp@gmail.com>
2025-12-11 03:02:58 +0700

    wallswitch.sh: add option to skip overview setup

--- private_dot_local/bin/executable_wallswitch.sh
+++ private_dot_local/bin/executable_wallswitch.sh
@@ -3,25 +3,49 @@ cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}"
 blur_img="${cache_dir}/niri/landing/blur"
 current_theme=$(dconf read /org/gnome/desktop/interface/color-scheme)
 
-if [[ ! -f "$1" ]]; then
-  echo "ERROR: $1 is not a valid file" >&2
-  exit 1
-fi
+switch_wallpaper() {
 
-if [[ "$1" != "$(readlink -f "$cache_dir/niri/landing/background")" ]]; then
-  if [ "$current_theme" = "'prefer-dark'" ]; then
-    matugen image "$1" -m dark
-  elif [ "$current_theme" = "'prefer-light'" ]; then
-    matugen image "$1" -m light
+  if [[ ! -f "$1" ]]; then
+    echo "ERROR: $1 is not a valid file" >&2
+    exit 1
   fi
 
-  ln -sf "$1" "${cache_dir}"/niri/landing/background
-  magick "$1" -blur 20x10 "${blur_img}"
-  # waypaper doesn't work properly with multiple swww-daemon instances
-  # swww img -n overview --transition-duration 2 -t fade "${blur_img}"
-  notify-send -e -r 2 -t 2000 "Wallpaper switch successful..." "Current Wallpaper: $(basename "$1")"
-else
-  echo "Same wallpaper detected. Skipping matugen & caching..."
-fi
+  if [[ "$1" != "$(readlink -f "$cache_dir/niri/landing/background")" ]]; then
 
-systemctl --user restart overview-blur.service
+    # fallback to prefer-light if color-scheme is default
+    if [ "$current_theme" = "'default'" ]; then
+      gsettings set org.gnome.desktop.interface color-scheme prefer-light
+    fi
+    if [ "$current_theme" = "'prefer-dark'" ]; then
+      matugen image "$1" -m dark
+    elif [ "$current_theme" = "'prefer-light'" ]; then
+      matugen image "$1" -m light
+    fi
+    ln -sf "$1" "${cache_dir}"/niri/landing/background
+    magick "$1" -blur 20x10 "${blur_img}"
+    notify-send -e -r 2 -t 2000 "Wallpaper switch successful..." "Current Wallpaper: $(basename "$1")"
+  else
+    echo "Same wallpaper detected. Skipping matugen & caching..."
+  fi
+
+  if [ ! "$SKIP_OVERVIEW" ]; then
+    systemctl --user restart overview-blur.service
+  else
+    echo "Skipping overview reloading..."
+  fi
+
+}
+
+while true; do
+  case "$1" in
+  --skip-overview)
+    SKIP_OVERVIEW=1
+    shift
+    ;;
+  *)
+    echo "Switching wallpaper: $1"
+    switch_wallpaper "$1"
+    break
+    ;;
+  esac
+done
