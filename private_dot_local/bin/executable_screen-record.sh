b0da10b27c27f3d450adb9a17050f10b6066d1db
SatanAlphabet <ori0pppppp@gmail.com>
2025-12-11 23:41:26 +0700

    screen-record.sh update
    - add option to record region
    - add module & menu entries to waybar

--- private_dot_local/bin/executable_screen-record.sh
+++ private_dot_local/bin/executable_screen-record.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
+video="${XDG_VIDEOS_DIR:-$HOME/Videos}/$(date "+%Y-%m-%d %H-%M-%S")"
+rec_args=(-c mp4 -o "$video")
+
 if ! pgrep -f gpu-screen-recorder >/dev/null; then
-  gpu-screen-recorder -w screen -o "$HOME/Videos/$(date "+%Y-%m-%d %H-%M-%S").mp4" >/dev/null 2>&1 &
-  echo "screen-recorder.sh: starting screen recording"
+  case "$1" in
+  region)
+    region="$(slurp -f "%wx%h+%x+%y")"
+    if [ -z "$region" ]; then
+      echo "screen-record.sh: no region selected."
+      exit 1
+    fi
+    rec_args+=(-w region -region "$region")
+    ;;
+  screen | *)
+    rec_args+=(-w screen)
+    ;;
+  esac
+  gpu-screen-recorder "${rec_args[@]}" >/dev/null 2>&1 &
+  echo "screen-record.sh: started recording"
 else
   pkill -SIGINT -f gpu-screen-recorder
-  echo "screen-recorder.sh: stopping recording"
+  echo "screen-record.sh: stopped recording"
   notify-send -a "Screen Recorder" "Screen Recorder" "Stopped video recording..." -e -t 3000 -i camera-video-symbolic
 fi
 
