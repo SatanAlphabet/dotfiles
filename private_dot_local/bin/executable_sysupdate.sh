79ece1c5a125524f64642df3b4ec44a27d0aaebc
SatanAlphabet <ori0pppppp@gmail.com>
2026-01-11 14:42:18 +0700

    sysupdate.sh: new no-confirm mode + general improvements

--- private_dot_local/bin/executable_sysupdate.sh
+++ private_dot_local/bin/executable_sysupdate.sh
@@ -6,39 +6,45 @@ NC='\033[0m' # No Color
 pkg_text=" - System packages updated."
 flatpak_text=" - Flatpak packages updated."
 post_upd_text=" - Post-update checks completed."
+
 upd_command=pacman
 
+upd_flags=(-Syu)
+flatpak_flags=(-u --system)
+post_upd_flags=(-i "sddm.service" -i "gdm.service") # Ignore display manager services to avoid nuking the current session
+
 fastfetch.sh
-echo -e """
 
- ${ORANGE}===>${NC}  Beginning system update...
+if [ "$1" = "-y" ]; then
+  upd_flags+=(--noconfirm)
+  flatpak_flags+=(-y)
+  post_upd_flags+=(-a)
+  echo -e " ${ORANGE}WARNING:${NC} Skipping confirmation prompts..."
+fi
 
-        """
+echo -e "\n ${ORANGE}===>${NC}  Beginning system update...\n"
+
 if which paru >/dev/null 2>&1; then
   upd_command=paru
 elif which yay >/dev/null 2>&1; then
   upd_command=yay
 fi
 
-if ! $upd_command -Syu; then
+if ! $upd_command "${upd_flags[@]}"; then
   failed_update=true
   pkg_text=" [!] Failed to update system packages."
 fi
-echo -e """
 
- ${ORANGE}===>${NC}  Checking for flatpak updates...
-        
-        """
-if ! flatpak update; then
+echo -e "\n${ORANGE}===>${NC}  Checking for flatpak updates...\n"
+
+if ! flatpak update "${flatpak_flags[@]}"; then
   failed_update=true
   flatpak_text=" [!] Failed to update flatpak packages."
 fi
-echo -e """
 
- ${ORANGE}===>${NC}  Running post-installation checks...
+echo -e "\n${ORANGE}===>${NC}  Running post-installation checks...\n"
 
-        """
-if ! sudo checkservices; then
+if ! sudo checkservices "${post_upd_flags[@]}"; then
   failed_update=true
   post_upd_text=" [!] Failed to run post-service checks."
 fi
