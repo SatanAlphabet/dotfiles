adc3a15e9b30cc8e68091e3eb67f1bb3a8cb34af
SatanAlphabet <ori0pppppp@gmail.com>
2026-02-02 21:36:00 +0700

    rework brightnesscontrol.sh and volumecontrol.sh
    -now pass arguments directly to brightnessctl and pactl respectively
    -update keybinds & waybar modules with new commands

--- private_dot_local/bin/executable_volumecontrol.sh
+++ private_dot_local/bin/executable_volumecontrol.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env bash
 
-case "$1" in
-+)
-  pactl set-sink-volume @DEFAULT_SINK@ +"${2}"%
-  ;;
--)
-  pactl set-sink-volume @DEFAULT_SINK@ -"${2}"%
-  ;;
-mute)
-  wpctl set-mute @DEFAULT_AUDIO_SINK@ 1
-  ;;
-unmute)
-  wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-  ;;
-toggle-mute)
-  wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-  ;;
-*)
-  exit 1
-  ;;
-esac
+pactl "$@"
 
 volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d\n", $2*100}')"%"
 mute_status=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%s\n", $3}')
