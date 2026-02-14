827a20a5eed947b3b2071e7db07d72eb7dd8355f
SatanAlphabet <ori0pppppp@gmail.com>
2026-02-14 14:39:03 +0700

    fix: add --source-color-index to matugen args
    part of matugen 4.0.0 breaking changes

--- private_dot_local/bin/executable_wallswitch.sh
+++ private_dot_local/bin/executable_wallswitch.sh
@@ -38,7 +38,7 @@ switch_wallpaper() {
       gsettings set org.gnome.desktop.interface color-scheme prefer-light
       current_theme="'prefer-light'"
     fi
-    matugen image "$1" -m "$(grep -oe 'light' -oe 'dark' <<<"$current_theme")" -t "$scheme" >/dev/null 2>&1 &
+    matugen image "$1" -m "$(grep -oe 'light' -oe 'dark' <<<"$current_theme")" --source-color-index 0 -t "$scheme" >/dev/null 2>&1 &
     ln -sf "$1" "$cache_dir/niri/landing/background"
 
     cache_img="$blur_cache"/"$(basename "$1")"
