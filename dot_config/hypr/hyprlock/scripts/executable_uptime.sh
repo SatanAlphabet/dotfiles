013458aca46ca100d3ee6aeecb424827b3726661
SatanAlphabet <ori0pppppp@gmail.com>
2026-02-16 00:59:35 +0700

    hyprlock: rework uptime.sh script
    now has a pretty option for better labels

--- dot_config/hypr/hyprlock/scripts/executable_uptime.sh
+++ dot_config/hypr/hyprlock/scripts/executable_uptime.sh
@@ -2,10 +2,38 @@
 
 uptime="$(cat /proc/uptime | awk '{printf ("%d",($1/1))}')"
 
-if [ "$uptime" -lt 3600 ]; then
-  awk '{printf("%dm\n",($1/60%60))}' <<<"$uptime"
-elif [ "$uptime" -lt 86400 ]; then
-  awk '{printf("%dh %02dm\n",($1/60/60%24),($1/60%60))}' <<<"$uptime"
-else
-  awk '{printf("%dd %dh %02dm\n",($1/60/60/24),($1/60/60%24),($1/60%60))}' <<<"$uptime"
-fi
+get_uptime() {
+  local day_label="d"
+  local hour_label="h"
+  local min_label="m"
+  days=$((uptime / 86400))
+  remainder=$((uptime % 86400))
+  hours=$((remainder / 3600))
+  remainder=$((remainder % 3600))
+  minutes=$((remainder / 60))
+
+  if [ $PRETTY_FORMAT ]; then
+    [ $days -gt 1 ] && day_label=" Days" || day_label=" Day"
+    [ $hours -gt 1 ] && hour_label=" Hours" || hour_label=" Hour"
+    [ $minutes -gt 1 ] && min_label=" Minutes" || min_label=" Minute"
+  fi
+
+  output="$minutes$min_label"
+  [ $hours -gt 0 ] && output="$hours$hour_label $output"
+  [ $days -gt 0 ] && output="$days$day_label $output"
+
+  echo "$output"
+}
+
+while true; do
+  case "$1" in
+  -p | --pretty)
+    PRETTY_FORMAT=true
+    shift
+    ;;
+  *)
+    get_uptime
+    break
+    ;;
+  esac
+done
