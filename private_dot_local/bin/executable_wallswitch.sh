41f1249be000c118eec8e9d26e6ed713732e70b3
SatanAlphabet <ori0pppppp@gmail.com>
2026-02-20 01:40:47 +0700

    wallswitch.sh: adjust backdrop brightness

--- private_dot_local/bin/executable_wallswitch.sh
+++ private_dot_local/bin/executable_wallswitch.sh
@@ -48,7 +48,7 @@ switch_wallpaper() {
     cache_img="$blur_cache"/"$(basename "$1")"
     [ ! -d "$blur_cache" ] && mkdir -p "$blur_cache"
     if [[ ! -e "$cache_img" || "$(basename "$cache_img")" != "$(basename "$1")" ]]; then
-      magick "$1" -blur 30x10 -brightness-contrast -20 "$cache_img"
+      magick "$1" -blur 30x10 -brightness-contrast -10 "$cache_img"
     fi
     ln -sf "$cache_img" "$blur_img"
 
