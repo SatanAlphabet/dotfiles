88932bdd141e6d2cab8bff42a4d3ad931a08564a
SatanAlphabet <ori0pppppp@gmail.com>
2026-03-23 09:24:03 +0700

    nightlight.sh: force low temp with custom sunrise-sunset time

--- private_dot_local/bin/executable_nightlight.sh
+++ private_dot_local/bin/executable_nightlight.sh
@@ -5,7 +5,7 @@ temp=${1:-4000}
 if pgrep -x wlsunset >/dev/null; then
   pkill wlsunset && notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Disabled..." -i night-light-symbolic
 else
-  wlsunset -t "$temp" >/dev/null 2>&1 &
+  wlsunset -S 00:00 -s 00:00 -t "$temp" >/dev/null 2>&1 &
   notify-send -a "Night Light" -e -t 2000 -r 1 "Night Light Enabled..." "Temperature: <b>${temp}K</b>" -i night-light-symbolic
 fi
 
