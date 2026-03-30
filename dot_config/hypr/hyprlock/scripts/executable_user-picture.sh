aa45b8f1911523fae205424b4c4c7aca88140d7d
SatanAlphabet <ori0pppppp@gmail.com>
2026-03-30 23:48:46 +0700

    hyprlock: rework user-picture script & change default user-icon path
    symlinks profile picture to one path instead of returning different
    paths
    
    allows for instant image display when no picture is found & fallback
    icon is used

--- dot_config/hypr/hyprlock/scripts/executable_user-picture.sh
+++ dot_config/hypr/hyprlock/scripts/executable_user-picture.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/hyprlock"
+cache_img="$cache_dir/user-icon"
+
 if [ -e "$HOME/.face" ]; then
-  echo "$HOME/.face"
+  user_img="$HOME/.face"
 elif [ -e "$HOME/.face.icon" ]; then
-  echo "$HOME/.face.icon"
+  user_img="$HOME/.face.icon"
 else
-  echo "${XDG_CONFIG_HOME:-$HOME/.config}/hypr/hyprlock/assets/default-icon"
+  user_img="${XDG_CONFIG_HOME:-$HOME/.config}/hypr/hyprlock/assets/default-icon"
 fi
+
+[ ! -d "$cache_dir" ] && mkdir -p "$cache_dir"
+[ ! -e "$cache_img" ] && ln -s "$user_img" "$cache_img"
+echo "$cache_img"
