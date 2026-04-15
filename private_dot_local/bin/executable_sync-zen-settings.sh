03f18c315240c405f086de81955798f888d0debe
SatanAlphabet <ori0pppppp@gmail.com>
2026-04-16 03:14:59 +0700

    sync-zen-settings.sh: use inputs instead of hardcoded paths

--- private_dot_local/bin/executable_sync-zen-settings.sh
+++ private_dot_local/bin/executable_sync-zen-settings.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
+file="$1"
+
 style_path="${XDG_CACHE_HOME:-$HOME/.cache}/matugen/zen-browser"
 [ -d "$HOME/.zen" ] && zen_dir="$HOME/.zen" || zen_dir="${XDG_CONFIG_HOME:-$HOME/.config}/zen"
 zen_settings="$zen_dir/installs.ini"
 zen_profile="$(awk -F "=" "/Default/"'{printf $2}' "$zen_settings")"
 if [[ -n "$zen_profile" && -d "$zen_dir/$zen_profile" ]]; then
   chrome_path="$zen_dir/$zen_profile/chrome"
-  if [ "$(readlink -f "$chrome_path/userChrome.css")" != "$style_path/userChrome.css" ]; then
-    ln -sf "$style_path/userChrome.css" "$chrome_path/userChrome.css"
-  fi
-  if [ "$(readlink -f "$chrome_path/userContent.css")" != "$style_path/userContent.css" ]; then
-    ln -sf "$style_path/userContent.css" "$chrome_path/userContent.css"
+  if [ "$(readlink -f "$chrome_path/$file")" != "$style_path/$file" ]; then
+    ln -sf "$style_path/$file" "$chrome_path/$file"
   fi
 fi
