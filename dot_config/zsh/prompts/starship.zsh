9fc9eb52ee91bdcb188824d06c4576ee94359cdd
SatanAlphabet <ori0pppppp@gmail.com>
2026-04-25 00:45:23 +0700

    zsh: add missing starship config & xdg fallback

--- dot_config/zsh/prompts/starship.zsh
+++ dot_config/zsh/prompts/starship.zsh
@@ -2,6 +2,6 @@
 
 if command -v starship &>/dev/null; then
   eval "$(starship init zsh)"
-  export STARSHIP_CACHE=$XDG_CACHE_HOME/starship
-  export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
+  export STARSHIP_CACHE=${XDG_CACHE_HOME:-$HOME/.cache}/starship
+  export STARSHIP_CONFIG=${XDG_CONFIG_HOME:-$HOME/.config}/starship/starship.toml
 fi
