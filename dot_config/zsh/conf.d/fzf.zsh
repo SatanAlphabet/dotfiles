41adadd2d3739a49b68af7f47702f8e4a25c8c7d
SatanAlphabet <ori0pppppp@gmail.com>
2026-09-26 02:21:13 +0700

    zsh(fzf): improve default settings
    similar to 13ab8d82

--- dot_config/zsh/conf.d/fzf.zsh
+++ dot_config/zsh/conf.d/fzf.zsh
@@ -41,19 +41,21 @@ _fuzzy_edit_search_file() {
 alias ffe='_fuzzy_edit_search_file' \
 
 # Default fzf options
-export FZF_DEFAULT_OPTS="   --border=rounded --border-label-pos=6 \
+export FZF_DEFAULT_COMMAND="fd --strip-cwd-prefix -E .git -E node_modules -E target -E __pycache__"
+export FZF_DEFAULT_OPTS="--border=rounded --border-label-pos=6 \
   --margin=0 --padding=1 --style=minimal\
   --layout=reverse --info=default \
   --marker=  --prompt='[$USER@fzf]$ ' \
   --height=100% --list-label=' Result '\
-  --bind='focus:transform-preview-label:[[ -n {} ]] && printf \" Previewing [%s] \" {}' \
+  --bind='focus:transform-preview-label:[ -n {} ] && printf \" Previewing [%s] \" {}' \
   --preview-window=60%,border-rounded,right \
-  --walker-skip .git,node_modules \
+  --color=base16,info:magenta,pointer:magenta \
+  --color=hl:blue,hl+:bright-blue:underline
   "
 
 # Keybind-specific fzf options
 export FZF_ALT_C_OPTS="--preview='tree -C {} | head -n 300' --border-label='| Change Directory |'"
 export FZF_CTRL_R_OPTS="--border-label='| Command History |'"
 export FZF_CTRL_T_OPTS="--border-label='| Search |'"
-export FZF_CTRL_T_COMMAND='fd -H -E .git -E node_modules -E .venv -E target'
-export FZF_ALT_C_COMMAND='fd -t d -H -E .git -E node_modules -E .venv -E target -E __pycache__'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND -t f"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND -t d"
